b2095822055808fcf9cdf7e6eee1e6cf416dfbf4
Petr Musil <petr0musil@gmail.com>
2023-09-18 09:04:56 +0200

    Exception handling enhanced. Comments added. README.md modified - setup description changed.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ## General info
 
-The Ministerstvo školství, mládeže a tělovýchovy privides registers of schools and school organizations on the  data.gov.cz portal (Portál otevřených dat). Anyone can get xml file - list of schools for particular district (kraj). This library helps to parse and convert xml file to csv file - the whole xml file or based on user defined address (city/town/village) search.
+The Ministerstvo školství, mládeže a tělovýchovy privides registers of schools and school organizations on the  data.gov.cz portal (Portál otevřených dat). Anyone can get xml file - list of schools for particular district (kraj - NUTS 3). This library helps to parse and convert xml file to csv file - the whole xml file or based on user defined address (city/town/village) or postcode search.
 
 ## Technologies
 
@@ -18,19 +18,17 @@ The Ministerstvo školství, mládeže a tělovýchovy privides registers of sch
 
 ## Setup
 
-Clone the repo:
+1. Clone the repo:
 https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository
 
-Go to the data.gov.cz and download the registry file of your district (kraj):
+2. Go to the data.gov.cz and download the registry file of your district (kraj):
 For example: https://data.gov.cz/datov%C3%A1-sada?iri=https%3A%2F%2Fdata.gov.cz%2Fzdroj%2Fdatov%C3%A9-sady%2F00022985%2F2cd115ecd204aadabe9dab96d4c3733c. And download the source file in xml to the root directory of the repo.
 
-In the command line go to the directory with the repo and use like this:
-"python xml-to-csv.py name_of_the_source_file name_of_the_city"
+3. In the command line go to the directory with the repo and use like this:
+First option - search based on address: "python xml-to-csv.py name_of_the_source_file name_of_the_city" --> "python xml-to-csv.py vrejcz020.xml Kladno". The search is base on xml attribute "MistoAdresa2", which should contain postcode (PSČ) and the name of city.
+Second option - whole source file to .csv: "python xml-to-csv.py name_of_the_source_file" --> "python xml-to-csv.py vrejcz020.xml"
 
-For example:
-"python xml-to-csv.py vrejcz020.xml Kladno"
-
-Csv file with the name "output.csv" from the registry is saved in the working directory.
+4. Csv file with the name "output.csv" from the registry is saved in the working directory.
 
 Due to the structure of the source xml file, you can use only those provided by the MŠMT.
 
--- xml-to-csv.py
+++ xml-to-csv.py
@@ -1,70 +1,88 @@
-"""
-Číselník pro SkolaDruhTyp:
-http://stistko.uiv.cz/katalog/cslnk.asp?aad=&aak=&aap=on&idc=AKDT&ciselnik=Druhy+a+typy+%9Akol+a+%9Akolsk%FDch+za%F8%EDzen%ED+%28nov%E9%29&poznamka=
-Jde o číselník typu školy
-"""
-
-# Importing the required libraries
 import xml.etree.ElementTree as Xet
 import pandas as pd
 import sys
 
+
 class XmlToCsvConverter:
-    def __init__(self, source_file, address = None) -> None:
-        self.cols = ["skola_nazev_typu", "skola_druh_typ", "zrizovatel", "ulice", "obec", "obec_2"]
+    def __init__(self, source_file, address=None) -> None:
+        self.cols = ["skola_nazev_typu", "skola_druh_typ",
+                     "zrizovatel", "ulice", "obec", "obec_2"]
         self.rows = []
         self.address = address
-        # Parsing the XML file
         self.xmlparse = Xet.parse(source_file)
         self.root = self.xmlparse.getroot()
         self.subjekty = self.root.findall("PravniSubjekt")
-   
+
     def append_row(self, skola_nazev_typu, skola_druh_typ, zrizovatel, adr_1, adr_2, adr_3):
-        self.rows.append({
-            "skola_nazev_typu": skola_nazev_typu,
-            "skola_druh_typ": skola_druh_typ,
-            "zrizovatel": zrizovatel,
-            "ulice": adr_1,
-            "obec": adr_2,
-            "obec_2": adr_3
-        })
-    
+        try:
+            self.rows.append({
+                "skola_nazev_typu": skola_nazev_typu,
+                "skola_druh_typ": skola_druh_typ,
+                "zrizovatel": zrizovatel,
+                "ulice": adr_1,
+                "obec": adr_2,
+                "obec_2": adr_3
+            })
+        except Exception as e:
+            print(
+                f"Something went wrong with the appending of the results to the list: {e}")
+            sys.exit(1)
+
     def convert(self):
-        for subjekt in self.subjekty:
-            zarizeni = subjekt.findall("SkolyZarizeni")
-            reditelstvi = subjekt.find("Reditelstvi")
-            reditelstvi_nazev = reditelstvi.find("RedPlnyNazev")
-            zrizovatel = reditelstvi_nazev.text
-            for skola in zarizeni :
-                konkretni_skoly = skola.findall("SkolaZarizeni")
-                for konkretni_skola in konkretni_skoly:
-                    skola_nazev_typu = konkretni_skola.find("SkolaPlnyNazev").text
-                    skola_druh_typ = konkretni_skola.find("SkolaDruhTyp").text
-                    # TO DO: There might be more places
-                    misto = konkretni_skola.find("SkolaMistaVykonuCinnosti").find("SkolaMistoVykonuCinnosti")
-                    adr_1 = misto.find("MistoAdresa1").text
-                    adr_2 = misto.find("MistoAdresa2").text
-                    adr_3 = misto.find("MistoAdresa3").text
-                    if self.address:
-                        if adr_2.find(self.address) >= 0:
-                            self.append_row(skola_nazev_typu, skola_druh_typ, zrizovatel, adr_1, adr_2, adr_3)
-                    else:
-                        self.append_row(skola_nazev_typu, skola_druh_typ, zrizovatel, adr_1, adr_2, adr_3)
-                    df = pd.DataFrame(self.rows, columns=self.cols)
-  
-                    # Writing dataframe to csv
-                    df.to_csv('output.csv')
+        try:
+            for subjekt in self.subjekty:
+                zarizeni = subjekt.findall("SkolyZarizeni")
+                reditelstvi = subjekt.find("Reditelstvi")
+                reditelstvi_nazev = reditelstvi.find("RedPlnyNazev")
+                zrizovatel = reditelstvi_nazev.text
+                for skola in zarizeni:
+                    konkretni_skoly = skola.findall("SkolaZarizeni")
+                    for konkretni_skola in konkretni_skoly:
+                        skola_nazev_typu = konkretni_skola.find(
+                            "SkolaPlnyNazev").text
+                        skola_druh_typ = konkretni_skola.find(
+                            "SkolaDruhTyp").text
+                        misto = konkretni_skola.find("SkolaMistaVykonuCinnosti").find(
+                            "SkolaMistoVykonuCinnosti")
+                        adr_1 = misto.find("MistoAdresa1").text
+                        adr_2 = misto.find("MistoAdresa2").text
+                        if misto.find("MistoAdresa3").text:
+                            adr_3 = misto.find("MistoAdresa3").text
+                        else:
+                            adr_3 = ""
+                        # Searching base on user city/village name or postcode
+                        if self.address:
+                            if adr_2.find(self.address) >= 0 or adr_3.find(self.address) >= 0:
+                                self.append_row(
+                                    skola_nazev_typu, skola_druh_typ, zrizovatel, adr_1, adr_2, adr_3)
+                        # Simple conversion of xml to csv
+                        else:
+                            self.append_row(
+                                skola_nazev_typu, skola_druh_typ, zrizovatel, adr_1, adr_2, adr_3)
+                        # Pandas dataframe
+                        df = pd.DataFrame(self.rows, columns=self.cols)
+                        # Writing dataframe to csv
+                        df.to_csv('output.csv')
+        except Exception as e:
+            print(f"Something went wrong in the converter method: {e}")
+
 
 if __name__ == "__main__":
     try:
+        # Two arguments provided by the user - search base on city/village/postcode
         if len(sys.argv) == 3:
             converter = XmlToCsvConverter(sys.argv[1], sys.argv[2])
+        # One argument provided by the user - simple conversion
         elif len(sys.argv) == 2:
             converter = XmlToCsvConverter(sys.argv[1])
+        # More arguments - rase error
         elif len(sys.argv) > 3:
             raise SystemExit(2)
+        # Conversion itself
         converter.convert()
     except FileNotFoundError:
         print("The file doesn´t exist!")
-    except SystemExit:
-        print("Don´t send more then two arguments, please: 1. name of the source file, 2. address")
+    except SystemExit as ex:
+        if SystemExit == 2:
+            print(
+                "Don´t send more then two arguments, please: 1. name of the source file, 2. address")
